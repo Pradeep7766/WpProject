6a56cb185b75b80751f560f061d5821599d8725e
Pradeep7766 <pradeepingle7766@gmail.com>
2024-08-18 13:55:35 +0530

    commit

--- WP PROJECTS/login/login.js
+++
@@ -1,6 +0,0 @@
-// script.js
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Add your login logic here
-    console.log('Login form submitted');
-});
